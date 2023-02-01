28376c539ff326c82383d2887e309ee07313bb7a
Miles Bosch <milbosrej@alu.edu.gva.es>
2023-02-01 11:06:23 +0100

    Cambios de Miles

--- src/helloEclipse/helloEclipse.java
+++ src/helloEclipse/helloEclipse.java
@@ -5,6 +5,7 @@ public class helloEclipse {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
+		System.out.println("Que pasa gente estoy doramion");
 	}
 
 }
